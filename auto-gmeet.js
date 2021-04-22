4f5e261963de09f16652966f4b685a8690b148fe
Zhanho Shing <zhanhoshing@icloud.com>
2021-04-22 17:36:16 +0800

    feat: auto refresh each 30 seconds

--- auto-gmeet.js
+++ auto-gmeet.js
@@ -1,25 +1,40 @@
 // ==UserScript==
 // @name         Auto Google Meet
 // @namespace    http://github.com/ShingZhanho/auto-gmeet.js
-// @version      0.1
-// @description  Auto refresh google meet if the meet is not yet started.
+// @version      0.1.1
+// @description  Automatically refresh google meet.
 // @author       Z. H. Shing
 // @match        https://meet.google.com/_meet/*
 // @icon         https://www.google.com/s2/favicons?domain=google.com
 // @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
-(function() {
+(async function() {
     // gets information
     let paras = new URLSearchParams(window.location.search);
     let authuser = paras.get('authuser');
     let hs = paras.get('hs');
     let alias = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1);
-    let requestUrl = 'https://meet.google.com/_meet/whoops?authuser=' + authuser + '&hs=' + hs + '&alias=' + alias;
+    if (alias == 'whoops') alias = paras.get('alias'); // auto correction
+    let requestUrl = 'https://meet.google.com/_meet/whoops?authuser=' + authuser + '&hs=' + hs + '&sc=256&alias=' + alias;
 
-    fetch(requestUrl)
-    .then(response => {
-        if (response.status == 404) location.reload();
-    });
+    let meetStarted = false;
 
+    let response = await fetch(requestUrl);
+    meetStarted = response.status !== 404;
+
+    // If lesson has not yet started
+    if (!meetStarted) {
+        document.querySelector('div.jtEd4b').textContent = "此會議尚未開始";
+        for (let i = 0; i <= 30; i++) {
+            document.querySelector('div.fwk7ze').textContent = "將於" + (30 - i) + "秒後重新整理網頁。";
+            await sleep(1000);
+        }
+        location.reload();
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 })();
